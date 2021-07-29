5984e57635291991bc72bf46ed7aee7ede56c327
Darul123 <darulh123@gmail.com>
2021-07-29 07:55:12 +0700

    Tugas Akhir Web Frontend

---
+++ function.js
@@ -0,0 +1,14 @@
+function pink() {
+    document.getElementById("body").style.backgroundColor = "#ffe4f6";
+    document.getElementById("nav-akhir").style.backgroundColor = "#ff95dd";
+    document.getElementById("focus").style.color = "#fe00ad";
+    document.getElementById("button").style.backgroundColor = "#ff95dd";
+    document.getElementById("main-image").src="Asset/img_2_2.png";
+};
+function biru() {
+    document.getElementById("body").style.backgroundColor = "#ebf6ff";
+    document.getElementById("nav-akhir").style.backgroundColor = "#10b8d1";
+    document.getElementById("focus").style.color = "#1078d1";
+    document.getElementById("button").style.backgroundColor = "#10b8d1";
+    document.getElementById("main-image").src="Asset/img_2.png";
+}
